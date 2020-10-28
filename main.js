4257ca2a698001fe2d6872bc10e756c3923e323d
yonghan <wangyh.92@gmail.com>
2020-10-28 09:53:55 +0800

    after going through

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 
+//configure the env
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
 
 //create an instance of express
@@ -13,22 +14,30 @@ app.set('view engine', 'hbs')
 app.set('views', __dirname + '/views')
 
 /***********Dynamic************/
-app.use(express.static(__dirname + '/dice_images')) //load the image files, next time use public in order to access css/images/etc.
 
 const imageArray = ['dado-1','roll2','three_dots','four','Five-Image','dice-showing-6'] 
-let randomImage = imageArray[Math.floor(Math.random()*6)];
-let randomImage2 = imageArray[Math.floor(Math.random()*6)];
+const roll_dice = () => Math.floor(Math.random()*6)
+
+//access to css/images/etc.
+app.use(express.static(__dirname + '/dice_images')) 
+app.use(express.static(__dirname + '/public')) 
+
+app.get(['/','/index.html'],(req,res)=>{
+    res.status(200)
+    res.type('text/html')
+    res.render('dice')
+})
 
 app.get('/roll',(req,res)=>{
     res.status(200)
     res.type('text/html')
-    res.render('roll',{ranImage: randomImage, ranImage2: randomImage2})
+    let randomImage = imageArray[roll_dice()];
+    let randomImage2 = imageArray[roll_dice()];
+    res.render('roll',{randomImage, randomImage2}) //key name and variable name are the same
 })
 
 app.use((req,res)=>{
-    res.status(200)
-    res.type('text/html')
-    res.render('dice')
+    res.redirect('/')
 })
 
 /*********************************/
